d7855621e4e9fc7128e319d76b7d7ecc491b0ce3
Samuel Pertiller <122675724+MemesBananza@users.noreply.github.com>
2024-08-19 20:27:03 +0200

    - added float left to the images (Products) - -removed prodimgs (index) - changed text to logo (both) - !Added GSAP (Unfinished)

---
+++ js/script.js
@@ -0,0 +1,16 @@
+gsap.registerEffect({
+    name: "fade",
+    defaults: {duration: 2}, //defaults get applied to the "config" object passed to the effect below
+    effect: (targets, config) => {
+        return gsap.to(targets, {duration: config.duration, opacity: 0});
+    }
+});
+
+//now we can use it like this:
+//gsap.effects.fade(".box");
+
+document.querySelectorAll(".box").forEach(function(box) {
+  box.addEventListener("mouseenter", function() {
+    gsap.effects.fade(this);
+  });
+});
